698e3371b067fead9b73e1f7f3af799499f020a9
baoliang.wan@aspentech.com <baoliang.wan@aspentech.com>
2019-12-19 13:44:23 +0800

    add two service

---
+++ spring-boot-microservice-forex-service/src/main/java/com/aspentech/springboot/SpringBootMicroserviceForexServiceApplication.java
@@ -0,0 +1,18 @@
+package com.aspentech.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+
+
+
+@SpringBootApplication
+public class SpringBootMicroserviceForexServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMicroserviceForexServiceApplication.class, args);
+	}
+
+}
