cef0f881308bc62379366814dc101c668ab1a1ee
baoliang.wan@aspentech.com <baoliang.wan@aspentech.com>
2019-12-19 13:45:29 +0800

    add two services

---
+++ spring-boot-microservice-currency-conversion/src/test/java/com/aspentech/springboot/SpringBootMicroserviceCurrencyConversionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aspentech.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMicroserviceCurrencyConversionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
